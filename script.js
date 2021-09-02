cd4f11acd5d5ddc71c5f1891417b11579335eccc
Prakhar Shukla <imprakhar7@gmail.com>
2021-09-02 11:23:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const daysEl1 = document.getElementById("days1");
+/*const daysEl1 = document.getElementById("days1");
 const hoursEl1 = document.getElementById("hours1");
 const minsEl1 = document.getElementById("mins1");
-const secondsEl1 = document.getElementById("seconds1");
+const secondsEl1 = document.getElementById("seconds1");*/
 
 const tilak= "20 Nov 2021";
 const wedding= "28 Nov 2021";
@@ -26,21 +26,21 @@ function countdown() {
     const totalSeconds1 = (tilakDate - currentDate) / 1000;
 
     const days1 = Math.floor(totalSeconds1 / 3600 / 24);
-    const hours1 = Math.floor(totalSeconds1 / 3600) % 24;
+    /*const hours1 = Math.floor(totalSeconds1 / 3600) % 24;
     const mins1 = Math.floor(totalSeconds1 / 60) % 60;
-    const seconds1 = Math.floor(totalSeconds1) % 60;
+    const seconds1 = Math.floor(totalSeconds1) % 60;*/
     
     daysEl.innerHTML = days1 +'/'+ days;
-    hoursEl.innerHTML = formatTime(hours);
+    /*hoursEl.innerHTML = formatTime(hours);
     minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
+    secondsEl.innerHTML = formatTime(seconds);*/
 
    
 
-    daysEl1.innerHTML = days1;
+    /*daysEl1.innerHTML = days1;
     hoursEl1.innerHTML = formatTime(hours1);
     minsEl1.innerHTML = formatTime(mins1);
-    secondsEl1.innerHTML = formatTime(seconds1);
+    secondsEl1.innerHTML = formatTime(seconds1);*/
 }
 
 function formatTime(time) {
