451cbfda132692fe16d86a6eba4d53f10db598fa
Prakhar Shukla <imprakhar7@gmail.com>
2021-11-15 12:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,4 +33,44 @@ function formatTime(time) {
 // initial call
 countdown();
 
+
+const container = document.querySelector('countdown-container')
+const fireworks = new Fireworks(container, {
+      rocketsPoint: 50,
+      hue: { min: 0, max: 360 },
+      delay: { min: 15, max: 30 },
+      speed: 2,
+      acceleration: 1.05,
+      friction: 0.95,
+      gravity: 1.5,
+      particles: 50,
+      trace: 3,
+      explosion: 5,
+      autoresize: true,
+      brightness: { 
+        min: 50, 
+        max: 80,
+        decay: { min: 0.015, max: 0.03 }
+      }
+      mouse: { 
+        click: false, 
+        move: false, 
+        max: 3 
+      }
+      boundaries: { 
+        x: 50, 
+        y: 50, 
+        width: container.clientWidth, 
+        height: container.clientHeight 
+      }
+      sound: {
+        enable: true,
+        files: [
+          'explosion0.mp3',
+          'explosion1.mp3',
+          'explosion2.mp3'
+        ],
+        volume: { min: 1, max: 2 },
+      }
+});
 setInterval(countdown, 1000);
