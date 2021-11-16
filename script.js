e0fc33ab16eef50034e637d71980d01ab2d081f9
Prakhar Shukla <imprakhar7@gmail.com>
2021-11-16 21:32:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const tilak= "20 Nov 2021 09:00 AM";
-const wedding= "28 Nov 2021 09:00 AM";
+const tilak= "20 Nov 2021 01:00 PM";
+const wedding= "28 Nov 2021 01:00 PM";
 
 function countdown() {
     const weddingDate = new Date(wedding);
